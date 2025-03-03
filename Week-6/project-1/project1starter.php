d441fdeba8efe3cdaf37d5588b557e3894936d9c
Elliott <emilybeeley2020@gmail.com>
2025-03-02 22:21:30 -0500

    feat:validation

--- Week-6/project-1/project1starter.php
+++ Week-6/project-1/project1starter.php
@@ -16,36 +16,13 @@
 
     <legend>Sign in:</legend>
 
-    <label for = "email-input">Enter your email: </label>
-    <input type="email" name="email-name" id="email-id" required>
+    <label for = "email">Enter your email: </label>
+    <input type="email" name="email" id="email" required>
 
     <div>
 
     <label for = "password">Enter your password (8 characters minimum): </label>
     <input type="password" name="password" id="password" minlength = "8" required>
-
-    <?php
-
-        $globalPassword = '$2y$10$t7eY2wQCX.EQvQg.3S6N6eG1mMZwEl4GMRc8HeT738ADfQvVs.wHm';
-
-        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-            $passwordEntered = $_POST['password'];
-
-            if(password_verify($passwordEntered, $globalPassword)){
-
-                echo 'password is valid';
-
-            }else{
-
-                echo 'password is invalid';
-
-            }
-
-        }
-
-    ?>
-
 
 </fieldset>
 
@@ -55,19 +32,19 @@
 
     <label>What age are you? </label>
 
-    <input type="radio" name="0-12" id="0"><label>0-12 </label>
-    <input type="radio" name="13-17" id="1"><label>13-17 </label>
-    <input type="radio" name="18-22" id="2"><label>18-22 </label>
-    <input type="radio" name="23-27" id="3"><label>23-27 </label>
-    <input type="radio" name="28-32" id="4"><label>28-32 </label>
-    <input type="radio" name="33-37" id="5"><label>33-37 </label>
-    <input type="radio" name="38-42" id="6"><label>38-42 </label>
-    <input type="radio" name="43-47" id="7"><label>43-47 </label>
-    <input type="radio" name="48-52" id="8"><label>48-52 </label>
-    <input type="radio" name="53-57" id="9"><label>53-57 </label>
-    <input type="radio" name="58-62" id="10"><label>58-62 </label>
-    <input type="radio" name="63-67" id="11"><label>63-67 </label>
-    <input type="radio" name="68+" id="12"><label>68+ </label>
+    <input type="radio" name="age" id="0"><label>0-12 </label>
+    <input type="radio" name="age" id="1"><label>13-17 </label>
+    <input type="radio" name="age" id="2"><label>18-22 </label>
+    <input type="radio" name="age" id="3"><label>23-27 </label>
+    <input type="radio" name="age" id="4"><label>28-32 </label>
+    <input type="radio" name="age" id="5"><label>33-37 </label>
+    <input type="radio" name="age" id="6"><label>38-42 </label>
+    <input type="radio" name="age" id="7"><label>43-47 </label>
+    <input type="radio" name="age" id="8"><label>48-52 </label>
+    <input type="radio" name="age" id="9"><label>53-57 </label>
+    <input type="radio" name="age" id="10"><label>58-62 </label>
+    <input type="radio" name="age" id="11"><label>63-67 </label>
+    <input type="radio" name="age" id="12"><label>68+ </label>
 
     <div>
 
@@ -89,7 +66,7 @@
     <legend>Lets catch up!</legend>
 
     <label for = "thoughts">How are you feeling?</label>
-    <textarea id = "thoughts" name = "thoughts">
+    <textarea id = "thoughts" name = "thoughts" minlength = "20" maxlength = "300">
     </textarea>
 
     <div>
@@ -107,6 +84,109 @@
 
 </form>
 
-</body>
+<?php
+
+        $email = $_POST['email'];
+
+        function validateEmail($email){
+
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+                echo 'email is valid';
+
+            }else{
+
+                echo 'email is invalid';
+
+            }
+
+        }
+
+        $globalPassword = '$2y$10$t7eY2wQCX.EQvQg.3S6N6eG1mMZwEl4GMRc8HeT738ADfQvVs.wHm';
+
+        $passwordEntered = $_POST['password'];
+
+        function validate_password($passwordEntered, $globalPassword){
+
+        if(password_verify($passwordEntered, $globalPassword)){
+
+                echo 'password is valid';
+
+        }else{
+
+            echo 'password is invalid';
+
+        }
+
+        }
+
+        $age = $_POST['age'];
+
+        function validate_age($age){
+
+            if (in_array($age)){
+
+                echo 'valid input'
+
+            }else{
+
+                echo 'invalid input'
+
+            }
+
+        }
+
+        $gender = $_POST['gender'];
+
+        function validateGender($gender){
+
+            if (in_array($gender)){
+
+                echo 'valid input'
+
+            }else{
+
+                echo 'invalid input'
+
+            }
+
+        }
+
+        $text = $_POST['thoughts'];
+
+        function validate_text($text){
+
+            if (is_string($text)){
+
+                echo 'valid input'
+
+            }else{
+
+                echo 'invalid input'
+
+            }
+
+        }
+
+
+        $scale = ['scale'];
+
+        function validate_scale($scale){
+
+            if (is_numeric($scale)){
+
+                echo 'valid input'
+
+            }else{
+
+                echo 'invalid input'
+
+            }
+
+        }
+
+    ?>
+
+    </body>
 
 </html>
--- Week-6/project-1/results.php
+++ Week-6/project-1/results.php
@@ -7,29 +7,8 @@
 
     <form action="project1starter.php" method="get" class="survey">
 
-    <?php
 
-        $globalPassword = '$2y$10$t7eY2wQCX.EQvQg.3S6N6eG1mMZwEl4GMRc8HeT738ADfQvVs.wHm';
-
-        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-            $passwordEntered = $_POST['password'];
-
-            if(password_verify($passwordEntered, $globalPassword)){
-
-                echo 'password is valid';
-
-            }else{
-
-                echo 'password is invalid';
-
-            }
-
-        }
-
-    ?>
-
-        <button type = "Back" name = "back-button" id = "back-button">Back</button>
+      <button type = "Back" name = "back-button" id = "back-button">Back</button>
 
     </form>
 
